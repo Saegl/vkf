bdbf043a9db1b7bcec82ce3984e192ec594b678d
Saegl <saegl@protonmail.com>
2023-03-10 14:13:41 +0600

    Add access token expiration error

--- vkf/api.py
+++ vkf/api.py
@@ -1,3 +1,4 @@
+import sys
 import httpx
 
 from vkf.models import Friend
@@ -6,12 +7,26 @@ from vkf.models import Friend
 BASE_API = "https://api.vk.com/method/"
 
 
-def vk_method(method_name, params, access_token: str) -> httpx.Response:
-    return httpx.post(
+class AccessTokenExpired(Exception):
+    pass
+
+
+def vk_method(method_name, params, access_token: str) -> dict:
+    response = httpx.post(
         BASE_API + method_name,
         headers={"Authorization": f"Bearer {access_token}"},
         params={"v": "5.131"} | params,
-    )
+    ).json()
+
+    check_error(response)
+    return response
+
+
+def check_error(response: dict):
+    if "error" in response:
+        if response["error"]["error_code"] == 5:
+            sys.tracebacklimit = 0
+            raise AccessTokenExpired("Access token expired, get new with `vkf auth`")
 
 
 def get_friends(access_token, user_id: int):
@@ -23,10 +38,8 @@ def get_friends(access_token, user_id: int):
         },
         access_token,
     )
-    # print(response.json())
-    json = response.json()
 
-    for item in json["response"]["items"]:
+    for item in response["response"]["items"]:
         friend = Friend(
             first_name=item["first_name"],
             last_name=item["last_name"],
