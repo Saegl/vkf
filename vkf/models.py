845fa1bfb0fb48f4395f095a6422b4fa12f8881f
Saegl <saegl@protonmail.com>
2023-03-12 23:01:10 +0600

    Comment friend props

--- vkf/models.py
+++ vkf/models.py
@@ -10,5 +10,13 @@ class Friend(BaseModel):
     last_name: str
     country: str | None
     city: str | None
+
+    # Birth date can be in 3 possible formats
+    # None if bdate is hidden
+    # DD.MM if year is hidden
+    # DD.MM.YYYY otherwise
     bdate: str | None
+
+    # 1 female
+    # 2 male
     sex: int
